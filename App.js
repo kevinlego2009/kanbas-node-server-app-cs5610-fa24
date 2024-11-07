47e7166b7ca8870b67b4c7445fb32e53c4303eb9
kevinlego2009 <yu.dae@northeastern.edu>
2024-11-07 01:13:51 -0800

    commit

---
+++ App.js
@@ -0,0 +1,12 @@
+// const express = require('express')
+import express from 'express';
+import HelloRoutes from "./hello.js";
+import Lab5 from "./Lab5/index.js";
+
+const app = express();
+app.use(express.json());
+
+Lab5(app);
+HelloRoutes(app);
+
+app.listen(process.env.PORT || 4000);
