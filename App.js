55b3d0fa791a6e711c0a5fbcf2ab1e4f8253f370
kevinlego2009 <yu.dae@northeastern.edu>
2024-11-20 23:07:32 -0800

    adjust CORS settings

--- App.js
+++ App.js
@@ -13,12 +13,19 @@ import EnrollmentsRoutes from "./Kanbas/Enrollments/routes.js";
 
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: process.env.NETLIFY_URL || "http://localhost:3000",
+//   })
+// );
+
+// Allow all origins
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Define allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Define allowed headers
+}));
 
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
